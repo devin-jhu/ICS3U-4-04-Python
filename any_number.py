3cf595e12bda22862ea19f44aa7dc7cb6c0e4a11
Devin Jhu <devin.jhu@mths.ca>
2022-05-12 02:42:15 +0000

    code

--- any_number.py
+++ any_number.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
 
 # Created by Devin Jhu
-# Created on March 2022
+# Created on May 2022
 # The area and perimeter calculator
+
+import random
 
 
 def main():
-    # this program shows the sum of all numbers from 0 to number
-    counter = 0
-    sum = 0
+    # this function is a guessing game
+
+    counter = 1
+
+    print("The number game")
+    print("Guess a number between 1 and 9")
 
     # input
-    number = input("Enter number (integer): ")
+    random_number = random.randint(0, 9)
+    guess_number_string = input("Enter your guess: ")
 
     # process & output
-    try:
-        number_int = int(number)
-        while counter < number_int:
-            sum = sum + (counter + 1)
-            counter = counter + 1
-        print("The sum of all numbers to {0} is {1}".format(number_int, sum))
+    while True:
+        try:
+            guess = int(guess_number_string)
+            if guess == random_number:
+                break
+            elif guess > random_number:
+                print("Guess lower")
+            elif guess < random_number:
+                print("Guess higher")
+            else:
+                print("What happened")
 
-    except Exception:
-        print("Not a number.")
+        except Exception:
+            print("Not a number")
+
+        guess_number_string = input("Try again: ")
+        counter += 1
+
+    # output
+    print("You Win! It took {0} tries.".format(counter))
     print("\nDone.")
 
 
